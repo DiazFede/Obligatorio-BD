974bd8b0d653791baf910cd268ab41ce2d0ca846
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-11-08 08:30:16 -0300

    .

--- frontend/obligatorio-front/src/App.jsx
+++ frontend/obligatorio-front/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Home from './pages/Home';
 import Login from './pages/Login'
 import Help from './pages/Help';
+import Register from './pages/Register';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Login />} /> {/* Ruta a Login */}
           <Route path="/home" element={<Home />} /> {/* Ruta a Home */}
           <Route path="/help" element={<Help />} /> {/* Ruta a Help */}
+          <Route path="register" element={<Register />} /> {/* Ruta a Register */}
         </Routes>
       </Router>
     </>
---
+++ frontend/obligatorio-front/src/pages/Register/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderLogin from "../../components/HeaderLogin";
+
+const Register = () => {
+    return (
+        <>
+            <HeaderLogin />
+            <div className = {styles.registerForm}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Register;
