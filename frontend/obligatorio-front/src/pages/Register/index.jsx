d579ca786d8d42683c2ce715437cacdc6a59e5c6
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-11-08 09:08:54 -0300

    .

--- frontend/obligatorio-front/src/pages/Register/index.jsx
+++ frontend/obligatorio-front/src/pages/Register/index.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './index.module.css';
+import HeaderLogin from '../../components/HeaderLogin';
+import { createAlumno } from '../../apiservices/api';
 
 const Register = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     cedula: '',
     nombre: '',
@@ -20,82 +24,89 @@ const Register = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Datos de registro:', formData);
+    try {
+      await createAlumno(formData);
+      navigate('/home'); // Redirige a Home después del registro exitoso
+    } catch (error) {
+      console.error("Error al registrar el usuario:", error);
+    }
   };
 
   return (
-    <div className={styles.registerForm}>
-      <h2 className={styles.title}>Registro de Usuario</h2>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <label className={styles.label}>Cédula de identidad</label>
-        <input
-          type="text"
-          name="cedula"
-          placeholder="Cédula de identidad"
-          value={formData.cedula}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Nombre</label>
-        <input
-          type="text"
-          name="nombre"
-          placeholder="Nombre"
-          value={formData.nombre}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Apellido</label>
-        <input
-          type="text"
-          name="apellido"
-          placeholder="Apellido"
-          value={formData.apellido}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Correo electrónico</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Correo electrónico"
-          value={formData.email}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Fecha de nacimiento</label>
-        <input
-          type="date"
-          name="fechaNacimiento"
-          value={formData.fechaNacimiento}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Teléfono</label>
-        <input
-          type="tel"
-          name="telefono"
-          placeholder="Teléfono"
-          value={formData.telefono}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <label className={styles.label}>Contraseña</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Contraseña"
-          value={formData.password}
-          onChange={handleChange}
-          className={styles.input}
-        />
-        <button type="submit" className={styles.button}>
-          Registrar
-        </button>
-      </form>
-    </div>
+    <>
+      <HeaderLogin />
+      <div className={styles.registerForm}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>Cédula de identidad</label>
+          <input
+            type="text"
+            name="cedula"
+            placeholder="Cédula de identidad"
+            value={formData.cedula}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Nombre</label>
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Apellido</label>
+          <input
+            type="text"
+            name="apellido"
+            placeholder="Apellido"
+            value={formData.apellido}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Correo electrónico</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Correo electrónico"
+            value={formData.email}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Fecha de nacimiento</label>
+          <input
+            type="date"
+            name="fechaNacimiento"
+            value={formData.fechaNacimiento}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Teléfono</label>
+          <input
+            type="tel"
+            name="telefono"
+            placeholder="Teléfono"
+            value={formData.telefono}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <label className={styles.label}>Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            value={formData.password}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>
+            Registrar
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
