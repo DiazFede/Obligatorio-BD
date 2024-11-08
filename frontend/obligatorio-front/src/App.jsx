797f057aa4fd99f07a2abc69de2ba64653fe3877
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-11-08 10:14:53 -0300

    .

--- frontend/obligatorio-front/src/App.jsx
+++ frontend/obligatorio-front/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login'
 import Help from './pages/Help';
 import Register from './pages/Register';
+import MyAccount from './pages/MyAccount';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<Home />} /> {/* Ruta a Home */}
           <Route path="/help" element={<Help />} /> {/* Ruta a Help */}
           <Route path="register" element={<Register />} /> {/* Ruta a Register */}
+          <Route path="/myaccount" element={<MyAccount />} /> {/* Ruta a MyAccount */}
         </Routes>
       </Router>
     </>
