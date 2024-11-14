4619d0b5cb300284b044dfa22abc0e049ef9226e
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-11-14 15:54:09 -0300

    Se corrige la posición del botón 'Home' en la página Help

--- frontend/obligatorio-front/src/components/HeaderHelp/index.jsx
+++ frontend/obligatorio-front/src/components/HeaderHelp/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from './index.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderHelp = () => {
+
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className={styles.header}>
+      <button onClick={handleHomeClick} className={styles.homeButton}>
+        <FontAwesomeIcon icon={faHouse} style={{ color: 'white' }} />
+      </button>
+
       <h2 className={styles.title}>Preguntas frecuentes</h2>
+      
+      <button className={styles.hiddenButton}>
+        <FontAwesomeIcon icon={faHouse} style={{ color: 'white' }} />
+      </button>
     </div>
   );
 };
--- frontend/obligatorio-front/src/pages/Help/index.jsx
+++ frontend/obligatorio-front/src/pages/Help/index.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Card from '../../components/Card';
 import styles from './index.module.css';
 import HeaderHelp from '../../components/HeaderHelp';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const Help = () => {
-    const navigate = useNavigate();
-
-    const handleHomeClick = () => {
-        navigate('/');
-    };
 
     return (
         <>
@@ -21,9 +13,6 @@ const Help = () => {
                 <Card title="¿Por qué no puedo anotarme a clase?" content="Revisa si hay clases disponibles." />
                 <Card title="¿Por qué no puedo ver los turnos disponibles?" content="Verifica que hayas iniciado sesión correctamente." />
             </div>
-            <button onClick={handleHomeClick} className={styles.homeButton}>
-                <FontAwesomeIcon icon={faHouse} style={{ color: 'white' }} />
-            </button>
         </>
     );
 }
