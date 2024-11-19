f5b03c29d61d5417db5fac3595c81194e5de28fe
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-11-19 09:48:10 -0300

    Se agrega opción para volver a Home desde MyAccount

--- frontend/obligatorio-front/src/pages/MyAccount/index.jsx
+++ frontend/obligatorio-front/src/pages/MyAccount/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../contexts/UserContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from './index.module.css';
 
 const MyAccount = () => {
@@ -13,22 +15,32 @@ const MyAccount = () => {
       navigate("/");
     }
 
-    }, [navigate, user]);
+  }, [navigate, user]);
+
+  const handleBackClick = () => {
+    navigate('/home');
+  }
 
   return (
-    <div className={styles.accountContainer}>
-      {user ? (
-        <div className={styles.accountDetails}>
-          <h2>Mis Datos</h2>
-          <p><strong>Nombre:</strong> {user.nombre}</p>
-          <p><strong>Apellido:</strong> {user.apellido}</p>
-          <p><strong>Email:</strong> {user.correo_electronico}</p>
-          <p><strong>Fecha de Nacimiento:</strong> {user.fecha_nacimiento}</p>
-        </div>
-      ) : (
-        <p>Cargando datos...</p>
-      )}
-    </div>
+    <>
+      <button onClick={handleBackClick} className={styles.backButton}>
+        <FontAwesomeIcon icon={faArrowLeft} style={{ color: 'white' }} />
+      </button>
+      
+      <div className={styles.accountContainer}>
+        {user ? (
+          <div className={styles.accountDetails}>
+            <h2>Mis Datos</h2>
+            <p><strong>Nombre:</strong> {user.nombre}</p>
+            <p><strong>Apellido:</strong> {user.apellido}</p>
+            <p><strong>Email:</strong> {user.correo_electronico}</p>
+            <p><strong>Fecha de Nacimiento:</strong> {user.fecha_nacimiento}</p>
+          </div>
+        ) : (
+          <p>Cargando datos...</p>
+        )}
+      </div>
+    </>
   );
 };
 
